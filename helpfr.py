22ceada0133ad7f048501b99aebd57dbe5e848e0
Arya <76611488+arya232004@users.noreply.github.com>
2023-05-03 23:49:06 +0530

    Delete helpfr.py

--- helpfr.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Read the text file using pandas
-df = pd.read_csv('Arya7.txt', delimiter='\t', header=None)
-
-# Save the data to an Excel file
-df.to_excel('attendence.xlsx', index=False, header=False)
